a14b04d3150a462f4df2a020a9f47a7b2769a574
Carl Simpson <carl.simpson@maginus.com>
2020-10-27 05:21:02 +0000

    Working Base Framework

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,12 @@
 <?php
   class Pages extends Controller {
     public function __construct(){
-      $this->postModel = $this->model('Post');
+     
     }
     
     public function index(){
       $data = [
-        'title' => 'Welcome'
+        'title' => 'TheAcademyMVC',
       ];
      
       $this->view('pages/index', $data);
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <h1><?php echo $data['title']; ?></h1>
+  <p>This is the TheAcamemyMVC PHP framework. Please refer to the TraversyMVC documentation on how to use it</p>
+<a href="https://www.udemy.com/course/object-oriented-php-mvc">TraversyMVC</a>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
