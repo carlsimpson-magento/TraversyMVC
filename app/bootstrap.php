ef6e7f2d6800972811ea21129ef7b63362671a6a
Carl Simpson <carl.simpson@maginus.com>
2020-10-27 04:34:43 +0000

    Database Entries Working

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,9 @@
-sxsxsxsx
+<?php
+  // Load Config
+  require_once 'config/config.php';
+
+  // Autoload Core Libraries
+  spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+  });
+  
---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'academymvc');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://the-academy-mvc.test');
+  // Site Name
+  define('SITENAME', 'TheAcademyMVC');
